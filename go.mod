7accfd53b683323e7e2361da5a5c27c349ce4374
ttzztztz <ttzztztz@outlook.com>
2020-08-24 10:19:08 +0800

    test: ac automata test cases && go mod tidy

--- go.mod
+++ go.mod
@@ -10,7 +10,5 @@ require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/golang/protobuf v1.4.1
 	github.com/klauspost/compress v1.10.11 // indirect
-	github.com/segmentio/kafka-go v0.4.2
-	google.golang.org/protobuf v1.25.0
 	xorm.io/xorm v1.0.3
 )
---
+++ services/sensitive/sensitive_test.go
@@ -0,0 +1,32 @@
+package sensitive
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestAllowPublish(t *testing.T) {
+	acAutoMachine = NewAcAutoMachine()
+
+	allSensitiveWords := []string{
+		"sb",
+		"fuck",
+	}
+	for _, word := range allSensitiveWords {
+		trimmedWord := strings.Trim(word, " ")
+
+		if len(trimmedWord) > 0{
+			acAutoMachine.AddPattern(word)
+		}
+	}
+	acAutoMachine.Build()
+
+	if AllowPublish("you are an sb") != false {
+		t.Fail()
+	}
+
+	if AllowPublish("fack") != true {
+		t.Fail()
+	}
+}
+
