ed4458decef29f037e1684e858129bff09803ef9
chikasaki <1347283135@qq.com>
2020-08-24 16:40:37 +0800

    Add color attribute, persist message

--- models/danmu.go
+++ models/danmu.go
@@ -7,6 +7,7 @@ type Danmu struct {
 	UId        uint32 `xorm:"notnull comment('发送弹幕的用户id')"`
 	RoomId     uint32 `xorm:"notnull comment('弹幕所属房间id')"`
 	Visible    bool
-	Content    string    `xorm:"notnull default '' ('弹幕内容')"`
+	Content    string    `xorm:"notnull default '' comment('弹幕内容')"`
+	Color      string    `xorm:"notnull default 'black' comment('弹幕颜色')""`
 	CreateTime time.Time `xorm:"created"`
 }
--- services/db/persist.go
+++ services/db/persist.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"danmu/protobuf"
+	"danmu/services/rpc"
 	"fmt"
 	"github.com/golang/protobuf/proto"
 )
@@ -15,4 +16,20 @@ func PersistDanmu(buf []byte) {
 	}
 
 	fmt.Printf("will persist danmu %+v \n", message)
+
+	danmuRequest := protobuf.DanmuRequest{
+		Uid:     message.Uid,
+		RoomId:  message.Rid,
+		Visible: true,
+		Content: message.Content,
+		Color:   message.Color,
+	}
+
+	ds := rpc.DanmuService{}
+	res := &protobuf.DanmuChangeResponse{}
+	err = ds.AddDanmu(danmuRequest, res)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
