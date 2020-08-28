a89ec39763692efe449f144a2c46949b2cbe46f2
ttzztztz <ttzztztz@outlook.com>
2020-08-28 14:57:15 +0800

    feat: dev env

--- services/kafka/kafka.go
+++ services/kafka/kafka.go
@@ -3,19 +3,29 @@ package kafka
 import (
 	"context"
 	"github.com/Shopify/sarama"
+	"os"
 )
 
 var (
-	Version sarama.KafkaVersion
-	Broker  = []string{
-		"kafka:9092",
-		//"localhost:9093",
-		//"localhost:9094",
-	}
+	Version               sarama.KafkaVersion
+	Broker                []string
 	CancelCtx, CancelFunc = context.WithCancel(context.Background())
 )
 
 func init() {
+	devEnv := os.Getenv("DEV")
+	if devEnv == "1" {
+		Broker = []string{
+			"kafka:9092",
+		}
+	} else {
+		Broker = []string{
+			"localhost:9092",
+			"localhost:9093",
+			"localhost:9094",
+		}
+	}
+
 	if version, err := sarama.ParseKafkaVersion("2.6.0"); err != nil {
 		panic(err)
 	} else {
