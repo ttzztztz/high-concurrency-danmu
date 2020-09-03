4fdc82940a4c70d9dbb6fa5a9320549f6571ad7a
chikasaki <1347283135@qq.com>
2020-09-03 12:16:39 +0800

    tmp

--- frontend/src/network/servers.ts
+++ frontend/src/network/servers.ts
@@ -1,4 +1,4 @@
-const SERVER_0 = process.env.NODE_ENV === "production" ? "http://10.108.21.47:31888/" : "http://localhost:8888/";
+const SERVER_0 = process.env.NODE_ENV === "production" ? "http://101.133.209.70:8888/" : "http://localhost:8888/";
 
 const servers = [SERVER_0];
 export const getOneServer = () => {
