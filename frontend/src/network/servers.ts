9711cec97a6261efbf2fad640b3bd0f5ae27dd4a
ttzztztz <ttzztztz@outlook.com>
2020-09-03 12:54:12 +0800

    chore: url

--- frontend/src/network/servers.ts
+++ frontend/src/network/servers.ts
@@ -1,4 +1,4 @@
-const SERVER_0 = process.env.NODE_ENV === "production" ? "http://101.133.209.70:8888/" : "http://localhost:8888/";
+const SERVER_0 = process.env.NODE_ENV === "production" ? "http://101.133.130.112:31888/" : "http://localhost:8888/";
 
 const servers = [SERVER_0];
 export const getOneServer = () => {
